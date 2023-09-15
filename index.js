faad68155c28e02d6f8c549c740c3be3228863d0
Gian Singh <73843250+Giansingh4710@users.noreply.github.com>
2023-09-15 13:57:50 -0400

    dl yt

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
+const ytdl = require('ytdl-core')
 const express = require('express')
 const app = express()
+
+const cors = require('cors')
+const corsOptions = {
+  origin: 'http://127.0.0.1:1313',
+}
+app.use(cors(corsOptions))
+
 app.listen(1313)
-
-// const cors = require('cors')
-// const corsOptions = {
-//   origin: 'http://localhost:3000',
-// }
-// app.use(cors(corsOptions))
-
 app.get('/', (req, res) => {
-  res.json({
-    msg: 'Hello',
-  })
-  console.log('Hello Sent from server')
+  res.send('Get Received: ')
 })
+app.post('/', (req, res) => {
+  let data = req.body
+  res.send('Data Received: ' + JSON.stringify(data))
+})
+
+
+const url = 'https://www.youtube.com/watch?v=ungkoUHIs7Y'
+
+async function downloadVideo(url) {
+  if (!ytdl.validateURL(url)) {
+    console.log('URL not valid')
+    return
+  }
+  const info = await ytdl.getInfo(url)
+  const title = info.videoDetails.title
+  data = []
+  for (let vid of info.formats) {
+    data.push({
+      itag: vid.itag,
+      mimeType: vid.mimeType,
+      url: vid.url,
+      hasAudio: vid.hasAudio,
+      hasVideo: vid.hasVideo,
+    })
+  }
+  console.log(data.length)
+  return data
+}
+// downloadVideo(url)
