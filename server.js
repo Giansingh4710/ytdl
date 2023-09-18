233b1e5578849ec52cf9634a8cf6b8e5a5943f84
Gian Singh <73843250+Giansingh4710@users.noreply.github.com>
2023-09-18 19:33:39 -0400

    works ish

--- server.js
+++ server.js
@@ -1,41 +1,43 @@
 const ytdl = require('ytdl-core')
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser'); // To parse JSON data in the request body
+app.use(express.json()) // to support JSON-encoded bodies
+app.use(express.urlencoded()) // to support URL-encoded bodies
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://45.76.2.28');
+  res.setHeader('Access-Control-Allow-Origin', 'http://45.76.2.28')
   // You can set other CORS headers here as needed
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
+})
 app.listen(1313, () => {
-  console.log('Server is running on port 1313');
-});
-
-app.post('/', (req, res) => {
-  const postData = req.body; // This contains the data sent in the POST request body
-  console.log(postData)
-  res.status(200).json({ 
-    message: 'Data received successfully',
-    serverReceived : req.body,
-    //req: JSON.stringify(req)
-  });
+  console.log('Server is running on port 1313')
 })
 
+app.post('/', (req, res) => {
+  const postData = req.body
+  const url = postData.url
+  downloadVideo(url).then((data) => {
+    res.status(200).json({
+      serverReceived: req.body,
+      title: data.title,
+      formats: data.formats,
+    })
+  })
+})
 
 const url = 'https://www.youtube.com/watch?v=ungkoUHIs7Y'
 async function downloadVideo(url) {
   if (!ytdl.validateURL(url)) {
     console.log('URL not valid')
-    return
+    return {}
   }
   const info = await ytdl.getInfo(url)
   const title = info.videoDetails.title
-  data = []
+  const formats = []
   for (let vid of info.formats) {
-    data.push({
+    formats.push({
       itag: vid.itag,
       mimeType: vid.mimeType,
       url: vid.url,
@@ -43,11 +45,9 @@ async function downloadVideo(url) {
       hasVideo: vid.hasVideo,
     })
   }
-  console.log(data.length)
-  return data
+  return { title, formats }
 }
 
 app.get('/', (req, res) => {
   res.send('Get Received: ')
 })
-
